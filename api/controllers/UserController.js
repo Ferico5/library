9d0f21380bce99f06fcf00671c676d5db5641eac
ferico <huangferico@gmail.com>
2025-03-19 15:28:36 +0700

    npm i jsonwebtoken bcryptjs dotenv to add token when login and hash password

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -1,14 +1,22 @@
 const UserModel = require('../models/UserModel.js');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+require('dotenv').config();
+
+const SECRET_KEY = process.env.JWT_SECRET || 'supersecretkey';
 
 const createUser = async (req, res) => {
   try {
-    const existingUser = await UserModel.findOne({ email: req.body.email });
+    const { full_name, mobile_number, email, password, role } = req.body;
 
+    const existingUser = await UserModel.findOne({ email });
     if (existingUser) {
       return res.status(400).json({ msg: 'Email already exists, try another!' });
     }
 
-    const newUser = new UserModel(req.body);
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new UserModel({ full_name, mobile_number, email, password: hashedPassword, role });
     await newUser.save();
 
     res.status(201).json({ msg: 'User created!' });
@@ -22,15 +30,21 @@ const loginUser = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    const user = await UserModel.findOne({ email, password });
-
-    if (user) {
-      res.status(200).json({ msg: 'Login successful', user });
-    } else {
-      res.status(400).json({ msg: 'Invalid email or password!' });
+    const user = await UserModel.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ msg: 'Invalid email or password!' });
     }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ msg: 'Invalid email or password!' });
+    }
+
+    const token = jwt.sign({ id: user._id, role: user.role }, SECRET_KEY, { expiresIn: '1h' });
+
+    res.status(200).json({ msg: 'Login successful', token, user });
   } catch (error) {
-    console.log(error.message);
+    console.error(error.message);
     res.status(500).json({ msg: 'Server error' });
   }
 };
